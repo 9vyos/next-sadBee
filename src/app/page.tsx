fbee643b4bf9b40110c4a8910f71ad55acf04804
현준용 <hjy4649@cocen.com>
2023-05-16 10:44:58 +0900

    초기화

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from "./login";
-
-export default function main() {
-  return (
-    <>
-      <Login></Login>
-    </>
-  );
-}
--- src/app/product/[index]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function detail({ params }) {
-  return <div>{params.index}번 상품 디테일 페이지</div>;
-}
